8fcc19f0433d4822a9e923c5d7b0e4be3c03ef2a
Kalpesha2000 <pingalekalpesha@gmail.com>
2023-06-29 14:52:11 +0530

    updated code

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,21 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+const transactionRoutes = require("./routes/transactionRoutes");
+const statisticsRoutes = require("./routes/statisticsRoutes");
+const barChartRoutes = require("./routes/barChartRoutes");
+const combinedDataRoutes = require("./routes/combinedDataRoutes");
 
-const transactionRoutes = require('./routes/transactionRoutes');
-const statisticsRoutes = require('./routes/statisticsRoutes');
-const barChartRoutes = require('./routes/barChartRoutes');
-const combinedDataRoutes = require('./routes/combinedDataRoutes');
+app.use("/transactions", transactionRoutes);
+app.use("/statistics", statisticsRoutes);
+app.use("/barchart", barChartRoutes);
+app.use("/combineddata", combinedDataRoutes);
 
-
-app.use('/transactions', transactionRoutes);
-app.use('/statistics', statisticsRoutes);
-app.use('/barchart', barChartRoutes);
-app.use('/combineddata', combinedDataRoutes);
-
-
-const port = 3000;
+const port = 8000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
